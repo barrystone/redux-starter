13fec59390428769e11fa0c4be144f0fcb5e9c76
barrystone <barrystone3183@gmail.com>
2020-05-09 16:33:42 +0800

    Designing the Store

--- src/index.js
+++ src/index.js
@@ -3,15 +3,21 @@ import {
   bugAdded,
   bugRemoved,
   bugResolved,
+  bugAssignToUser,
+  getBugsByUser,
   getUnresolvedBugs,
 } from "./store/bugs";
 import { projectAdded } from "./store/projects";
+import { userAdded } from "./store/users";
 
 const store = configureStore();
 
 store.subscribe(() => {
   console.log("Store changed!");
 });
+
+store.dispatch(userAdded({ name: "user 1" }));
+store.dispatch(userAdded({ name: "user 2" }));
 
 store.dispatch(projectAdded({ name: "Project 1" }));
 
@@ -22,12 +28,10 @@ store.dispatch(bugAdded({ description: "Bug 4" }));
 store.dispatch(bugResolved({ id: 1 }));
 store.dispatch(bugRemoved({ id: 2 }));
 
-// Check array are the same
-const x = getUnresolvedBugs(store.getState());
-const y = getUnresolvedBugs(store.getState());
-console.log("x", x);
-console.log("y", y);
-console.log(x == y);
+store.dispatch(bugAssignToUser({ bugId: 1, userId: 1 }));
 
-// const unresolvedBugs = getUnresolvedBugs(store.getState());
-// console.log(unresolvedBugs);
+const unresolvedBugs = getUnresolvedBugs(store.getState());
+console.log(unresolvedBugs);
+
+const bugs = getBugsByUser(1)(store.getState());
+console.log(bugs);
--- src/store/bugs.js
+++ src/store/bugs.js
@@ -21,20 +21,30 @@ const slice = createSlice({
       const index = bugs.findIndex((bug) => bug.id === action.payload.id);
       bugs[index].resolved = true;
     },
+    bugAssignToUser: (bugs, action) => {
+      const idx = bugs.findIndex((bug) => bug.id === action.payload.bugId);
+      bugs[idx].userId = action.payload.userId;
+    },
   },
 });
 
-export const { bugAdded, bugRemoved, bugResolved } = slice.actions;
+export const {
+  bugAdded,
+  bugRemoved,
+  bugResolved,
+  bugAssignToUser,
+} = slice.actions;
 
 export default slice.reducer;
 
-// Selectors, version of no-reselect
-// export const getUnresolvedBugs = (state) =>
-//   state.entities.bugs.filter((bug) => !bug.resolved);
-
-// Memoizing Selectors with Reselect
 export const getUnresolvedBugs = createSelector(
   (state) => state.entities.bugs,
   (state) => state.entities.projects,
   (bugs, projects) => bugs.filter((bug) => !bug.resolved)
 );
+
+export const getBugsByUser = (userId) =>
+  createSelector(
+    (state) => state.entities.bugs,
+    (bugs) => bugs.filter((bug) => bug.userId === userId)
+  );
